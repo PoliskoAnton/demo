467361eb75820208238d733a2347b662f28ef47c
Anton <anton_polisko@gmail.com>
2025-08-28 10:35:37 +0200

    create again jwt authorization :)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // если нужно деплоить в поддиректории (на Render можно оставить ./)
+  base: '/', // если нужно деплоить в поддиректории (на Render можно оставить ./)
   server: {
     port: 5173,
     open: true

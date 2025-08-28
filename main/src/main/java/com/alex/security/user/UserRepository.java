45e32801722c9ba55d454aebc5e7c90902bf14cf
Anton <anton_polisko@gmail.com>
2025-08-28 11:38:02 +0200

    backend

--- main/src/main/java/com/alex/security/user/UserRepository.java
+++ main/src/main/java/com/alex/security/user/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    boolean existsByUsername(String username);
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
